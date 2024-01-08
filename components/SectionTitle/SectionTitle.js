51b35fd474a81839177ddf856410f6add41223a8
odaka-yuki <54489400+odaka-web@users.noreply.github.com>
2024-01-08 22:57:44 +0900

    SectionTitle

---
+++ components/SectionTitle/SectionTitle.js
@@ -0,0 +1,9 @@
+import styles from "./SectionTitle.module.css";
+
+const SectionTitle = ({ children }) => {
+	return (
+		<h2 className={styles.title}>{children}</h2>
+	);
+};
+
+export default SectionTitle;
